cbde9489cc9d82a7178f1fed7e1a69a74fbe2a4e
Adeline Izri <adeline.rudnicki@gmail.com>
2022-11-28 17:56:41 +0100

    Ajout theme light/dark + bouton de changement et ajout d'une navigation responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,70 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Portfolio';
+
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+
+  theme: string = "light-theme";
+  sidenavColor: string = "sidenav-light-color";
+
+  constructor(
+    private observer: BreakpointObserver
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.setDefaultTheme();
+  }
+
+  setDefaultTheme() {
+    if (localStorage.getItem('theme') && localStorage.getItem('sidenavColor')) {
+      this.theme = localStorage.getItem('theme') as string;
+      const body = document.getElementsByTagName('body')[0];
+      body.classList.add(this.theme);
+
+      this.sidenavColor = localStorage.getItem('sidenavColor') as string;
+      const sidenav = document.getElementsByTagName('mat-sidenav')[0];
+      sidenav.classList.add(this.sidenavColor);
+    }
+  }
+
+  switchTheme() {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove(this.theme);
+    this.theme === 'light-theme' ?
+                    this.theme = 'dark-theme' :
+                    this.theme = 'light-theme';
+    body.classList.add(this.theme);
+    localStorage.setItem('theme', this.theme);
+
+    const sidenav = document.getElementsByTagName('mat-sidenav')[0];
+    sidenav.classList.remove(this.sidenavColor);
+    this.sidenavColor === 'sidenav-light-color' ?
+                    this.sidenavColor = 'sidenav-dark-color' :
+                    this.sidenavColor = 'sidenav-light-color';
+    sidenav.classList.add(this.sidenavColor);
+    localStorage.setItem('sidenavColor', this.sidenavColor);
+  }
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 768px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
