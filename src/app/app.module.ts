269e4da90a7ecb01d6e02362c71a1d86972c0346
Adeline Izri <adeline.rudnicki@gmail.com>
2022-12-03 19:25:11 +0100

    Modification page contact avec MatCardModule + style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -37,6 +38,7 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatCardModule,
     MatDividerModule,
     MatIconModule,
     MatSidenavModule,
