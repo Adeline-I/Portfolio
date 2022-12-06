599df8186b8803a1a52c57e7c0993430c3ea2fa8
Adeline Izri <adeline.rudnicki@gmail.com>
2022-12-06 15:27:54 +0100

    Ajout de la page skills avec les images

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -9,13 +9,25 @@ import { SkillsService } from './skills.service';
 })
 export class SkillsComponent implements OnInit {
 
-  skillList!: Skill[];
+  cmsList!: Skill[];
+  databaseList!: Skill[];
+  designList!: Skill[];
+  devlopmentToolList!: Skill[];
+  frameworkList!: Skill[];
+  languageList!: Skill[];
+  projectManagementList!: Skill[];
 
   constructor(
     private skillsService: SkillsService
   ) { }
 
   ngOnInit(): void {
-    this.skillList = this.skillsService.getSkillsList();
+    this.cmsList = this.skillsService.getCmsList();
+    this.databaseList = this.skillsService.getDatabaseList();
+    this.designList = this.skillsService.getDesignList();
+    this.devlopmentToolList = this.skillsService.getDevlopmentToolList();
+    this.frameworkList = this.skillsService.getFrameworkList();
+    this.languageList = this.skillsService.getLanguageList();
+    this.projectManagementList = this.skillsService.getProjectManagementList();
   }
 }
--- src/app/pages/skills/skills.service.ts
+++ src/app/pages/skills/skills.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
-import { SKILLS } from 'src/app/datas/skills.data';
+import { CMS } from 'src/app/datas/skills/cms.data';
+import { DATABASE } from 'src/app/datas/skills/database.data';
+import { DESIGN } from 'src/app/datas/skills/design.data';
+import { DEVELOPMENT_TOOL } from 'src/app/datas/skills/development-tool.data';
+import { FRAMEWORK } from 'src/app/datas/skills/framework.data';
+import { LANGUAGE } from 'src/app/datas/skills/language.data';
+import { PROJECT_MANAGEMENT } from 'src/app/datas/skills/project-management.data';
 import { Skill } from 'src/app/models/skills.model';
 
 @Injectable({
@@ -9,7 +15,26 @@ export class SkillsService {
 
   constructor() { }
 
-  getSkillsList(): Skill[] {
-    return SKILLS;
+  getCmsList(): Skill[] {
+    return CMS;
   }
+  getDatabaseList(): Skill[] {
+    return DATABASE;
+  }
+  getDesignList(): Skill[] {
+    return DESIGN;
+  }
+  getDevlopmentToolList(): Skill[] {
+    return DEVELOPMENT_TOOL;
+  }
+  getFrameworkList(): Skill[] {
+    return FRAMEWORK;
+  }
+  getLanguageList(): Skill[] {
+    return LANGUAGE;
+  }
+  getProjectManagementList(): Skill[] {
+    return PROJECT_MANAGEMENT;
+  }
+
 }
